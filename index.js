870ce99a2607d49a813b4d0e56696d48d7790df9
oys <vkdnjdbstn@gmail.com>
2020-08-14 17:01:30 +0900

    정보보호

--- index.js
+++ index.js
@@ -2,22 +2,40 @@ const express = require("express");
 const app = express();
 const port = 5000;
 
+const { User } = require("./models/User");
+const bodyParser = require("body-parser");
+
+const config = require("./config/key");
+
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+//application/json
+app.use(bodyParser.json());
+
 const mongoose = require("mongoose");
 mongoose
-  .connect(
-    "mongodb+srv://youn:youn1210@boiler-plate.hnkur.mongodb.net/<dbname>?retryWrites=true&w=majority",
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-      useFindAndModify: false,
-    }
-  )
+  .connect(config.mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
   .then(() => console.log("MongoDB Connected..."))
   .catch((err) => console.log(err));
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Hello World! eyaaa~~");
+});
+
+app.post("/register", (req, res) => {
+  // 회원가입시 필요한 정보를 client에서 가져오면 DB에 넣어준다.
+  const user = new User(req.body);
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({
+      success: true,
+    });
+  });
 });
 
 app.listen(port, () => {
