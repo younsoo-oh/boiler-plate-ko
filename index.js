dbcc0073f906931f4f2a2f7cafc211b217bcc617
oys <vkdnjdbstn@gmail.com>
2020-08-18 01:39:54 +0900

    logout

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 
 const config = require("./config/dev");
+const { auth } = require("./middleware/auth");
 
 //application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -32,7 +33,7 @@ app.get("/", (req, res) => {
   res.send("Hello World! eyaaa~~");
 });
 
-app.post("/register", (req, res) => {
+app.post("/api/users/register", (req, res) => {
   // 회원가입시 필요한 정보를 client에서 가져오면 DB에 넣어준다.
   const user = new User(req.body);
   //여기에서 User.js에 암호화가 들어가고 밑에거 실행
@@ -44,7 +45,7 @@ app.post("/register", (req, res) => {
   });
 });
 
-app.post("/login", (req, res) => {
+app.post("/api/users/login", (req, res) => {
   //요청된 이메일이 DB에 있는지 확인
   User.findOne({ email: req.body.email }, (err, user) => {
     if (!user) {
@@ -70,6 +71,30 @@ app.post("/login", (req, res) => {
           .status(200)
           .json({ loginSuccess: true, userId: user._id });
       });
+    });
+  });
+});
+
+//role 0이면 일반유저, 0이아니면 관리자라고 임의로 설정
+app.get("/api/users/auth", auth, (req, res) => {
+  //여기까지 미들웨어를 통과했다는 것은 auth가 true라는 것
+  res.status(200).json({
+    _id: req.user._id,
+    isAdmin: req.user.role === 0 ? false : true,
+    isAuth: true,
+    email: req.user.email,
+    name: req.user.name,
+    lastname: req.user.lastname,
+    role: req.user.role,
+    image: req.user.image,
+  });
+});
+
+app.get("/api/users/logout", auth, (req, res) => {
+  User.findOneAndUpdate({ _id: req.user._id }, { token: "" }, (err, user) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).send({
+      success: true,
     });
   });
 });
--- models/User.js
+++ models/User.js
@@ -74,6 +74,23 @@ userSchema.methods.generateToken = function (cb) {
   });
 };
 
+userSchema.statics.findByToken = function (token, cb) {
+  let user = this;
+
+  //token을 decode한다.
+  jwt.verify(token, "secretToken", function (err, decoded) {
+    //유저 아이디를 이용해서 유저를 찾은 다음
+    //클라이언트에서 가져온 token과 DB에 보관된 토큰이 일치하는지 확인
+    user.findOne({ _id: decoded, token: token }, function (err, user) {
+      if (err) {
+        return cb(err);
+      } else {
+        cb(null, user);
+      }
+    });
+  });
+};
+
 const User = mongoose.model("User", userSchema);
 
 module.exports = { User };
